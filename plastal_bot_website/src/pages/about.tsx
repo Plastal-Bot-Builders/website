034fca306380c46c3eee250562b83571916d8ceb
fredrickmwepu <fredrickmwepu22@gmail.com>
2024-10-30 23:18:46 +0200

    started to work on the abouts page

--- plastal_bot_website/src/pages/about.tsx
+++ plastal_bot_website/src/pages/about.tsx
@@ -126,7 +126,19 @@ const about: React.FC = () => {
                 </span>
                 <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Introduction to Robotics and Programming Workshop</h3>
                 <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">18th May 2024</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Details of the workshop...</p>
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  We conducted the first workshop which introduced young students to the basics of programming and robotics. The workshop covered fundamental concepts such as algorithms, logic, and problem-solving techniques. Students were given hands-on experience with programming languages like Python and Scratch, allowing them to create simple programs and games. Additionally, they were introduced to robotics through interactive sessions where they built and programmed basic robots using kits like LEGO Mindstorms and Arduino. The workshop aimed to spark interest in STEM fields and equip students with essential skills for the future. By the end of the workshop, students demonstrated their projects, showcasing their newfound knowledge and creativity.
+                  </p>
+              </li>
+              <li className="mb-10 ms-6">
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                  <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                  </svg>
+                </span>
+                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Next Workshop</h3>
+                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Coming Soon</time>
+                <p className="text-base font-normal text-gray-500 dark:text-gray-400">Stay tuned for updates...</p>
               </li>
               <li className="mb-10 ms-6">
                 <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
