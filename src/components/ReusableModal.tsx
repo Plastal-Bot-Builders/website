a36c65b6c2d6148a3545adb5010cd1511d241d13
fredrickmwepu <fredrickmwepu22@gmail.com>
2025-10-14 23:02:23 +0200

    refinning the file structure

--- src/components/ReusableModal.tsx
+++ src/components/ReusableModal.tsx
@@ -12,7 +12,6 @@ interface ReusableModalProps {
     image?: string; // Optional image prop
 }
 
-
 const ReusableModal: React.FC<ReusableModalProps> = ({ 
     isOpen, 
     onClose, 
@@ -22,22 +21,34 @@ const ReusableModal: React.FC<ReusableModalProps> = ({
     image 
 }) => {
     return (
-     <Modal show={isOpen} onClose={onClose} className="flex items-center justify-center p-4 sm:p-8 md:p-16 lg:p-32 xl:p-48 2xl:p-64 backdrop-blur-sm">
+     <Modal 
+        show={isOpen} 
+        onClose={onClose} 
+        className="flex items-center justify-center p-4 sm:p-8 md:p-16 lg:p-32 xl:p-48 2xl:p-64 backdrop-blur-sm"
+        theme={{
+          content: {
+            base: "relative bg-card text-text rounded-lg shadow flex flex-col max-h-[90vh]",
+            inner: "relative p-4 md:p-5"
+          },
+          header: {
+            base: "flex items-start justify-between rounded-t border-b border-gray-400 dark:border-gray-600 p-5"
+          }
+        }}
+      >
             <div className="w-full max-w-4xl mx-auto overflow-y-auto max-h-screen p-4 md:p-5">
                 <div className="flex justify-end">
                     <button
                         type="button"
-                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        className="text-accent bg-transparent hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center transition-colors"
                         onClick={onClose}
-                        style={{ color: '#0cffbb' }} // Custom color
                     >
                         {FaTimes ({className: "w-3 h-3"})}
                         <span className="sr-only">Close modal</span>
                     </button>
                 </div>
                 <div className="text-center">
-                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">{title}</h3>
-                    {image && <img src={asset(image)} alt="Modal Image" className="mx-auto mb-4 w-32 h-32 object-cover" />}
+                    <h3 className="mb-5 text-lg font-normal text-text">{title}</h3>
+                    {image && <img src={asset(image)} alt="Modal Image" className="mx-auto mb-4 w-32 h-32 object-cover rounded-lg" />}
                     <div className="flex flex-col items-center justify-center">
                         {content}
                     </div>
